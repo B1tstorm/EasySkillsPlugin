ddc9686b41ae5f25fe340bb1e8ce6fac4239018d
B1tstorm <fokodiamond@gmail.com>
2022-10-22 20:25:38 +0200

    Added basic plugincode

--- src/main/java/de/onezerocoding/easyskills/EasySkills.java
+++ src/main/java/de/onezerocoding/easyskills/EasySkills.java
@@ -3,14 +3,45 @@ package de.onezerocoding.easyskills;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.configuration.file.FileConfiguration;
+
 
 public final class EasySkills extends JavaPlugin {
+
+    public FileConfiguration config = this.getConfig();
 
     @Override
     public void onEnable() {
         Bukkit.getConsoleSender().sendMessage(ChatColor.DARK_GREEN + "EasySkills runs");
+        printLegend();
+        setConfig();
+        registerEvents();
+        registerCommands();
+    }
 
+    private void printLegend() {
+        Bukkit.getConsoleSender().sendMessage(ChatColor.UNDERLINE + "Legend:");
+        Bukkit.getConsoleSender().sendMessage("- " + ChatColor.DARK_GREEN + "Dark green: onEnable()");
+        Bukkit.getConsoleSender().sendMessage("- " + ChatColor.DARK_RED + "Dark red: onDisable()");
+        Bukkit.getConsoleSender().sendMessage("- " + ChatColor.DARK_PURPLE + "Dark purple: Dev notes");
+        Bukkit.getConsoleSender().sendMessage("- " + ChatColor.YELLOW + "Yellow: Warning. Check it!");
+        Bukkit.getConsoleSender().sendMessage("- " + ChatColor.RED + "RED: Error. Exception!");
+        Bukkit.getConsoleSender().sendMessage("- " + ChatColor.LIGHT_PURPLE + "Light purple: Loaded from config");
+    }
 
+    private void setConfig() {
+        config.addDefault("owner", "Smaragd");
+        config.options().copyDefaults(true);
+        this.saveConfig();
+        Bukkit.getConsoleSender().sendMessage( "Owner of this server is " + ChatColor.LIGHT_PURPLE.toString() + config.get("owner"));
+    }
+
+    private void registerEvents() {
+        //getServer().getPluginManager().registerEvents(new EventMyListener(), this);
+    }
+
+    private void registerCommands() {
+        //this.getCommand("myCommand").setExecutor(new CmdMyCommand());
     }
 
     @Override
